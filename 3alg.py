Fixed average reward evaluation. Moved from Python sequences to NumPy arrays. Did some refactoring.

3alg.py
@@ -1,28 +1,29 @@
-#######################################################################
-# Usage: python 3alg.py <bandits> <arms> <iterations> <epsilon> <tau> #
-#######################################################################
-# Generates "graph.pdf" graph containing learning curves for          #
-# greedy, epsilon-greedy and softmax methods used to solve            #
-# n-armed bandit problem                                              #
-#######################################################################
+##########################################################################################
+# Usage: python 3alg.py <bandits> <arms> <iterations> <epsilon> <tau> <initial_estimate> #
+##########################################################################################
+# Generates "graph.pdf" graph containing learning curves for                             #
+# greedy, epsilon-greedy and softmax methods used to solve n-armed bandit problem        #
+##########################################################################################
 from bandit import Bandit
 from actors import Greedy
 from actors import EpsilonGreedy
 from actors import Softmax
 import sys
-import operator
 import matplotlib as mpl
 mpl.use('pdf')
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as pp
+import numpy as np
+import time
+import main
 
-pdf = PdfPages('graph.pdf')
-
+##########################################################################################
 bandits = 100
 arms = 10
 iterations = 1000
 epsilon = 0.1
 tau = 0.1
+init_estimate = 0
 if len(sys.argv) > 1:
     bandits = int(sys.argv[1])
     if len(sys.argv) > 2:
@@ -33,44 +34,28 @@ if len(sys.argv) > 1:
                 epsilon = float(sys.argv[4])
                 if len(sys.argv) > 5:
                     tau = float(sys.argv[5])
+                    if len(sys.argv) > 6:
+                        init_estimate = float(sys.argv[6])
 print "bandits:", bandits
 print "arms:", arms
 print "iterations:", iterations
+print "init_estimate:", init_estimate
 
-total_greedy_avg = [0 for _ in range(iterations)]
-total_e_greedy_avg = [0 for _ in range(iterations)]
-total_softmax_avg = [0 for _ in range(iterations)]
+##########################################################################################
+pdf = PdfPages('graph.pdf')
 
-greedy_avg = [0 for _ in range(iterations)]
-e_greedy_avg = [0 for _ in range(iterations)]
-softmax_avg = [0 for _ in range(iterations)]
+cl = time.clock()
+b = Bandit(arms)
+actors = [Greedy(b, init_estimate), EpsilonGreedy(epsilon, b, init_estimate), Softmax(tau, b, init_estimate)]
+avg = main.run(actors, bandits, arms, iterations)
+cl = time.clock() - cl
+print "time: " + str(cl) + " s"
 
-bdt = [Bandit(arms) for _ in range(bandits)]
-for b in bdt:
-    a = Greedy(b)   # Create greedy actor playing 10-armed bandit
-    for i in range(iterations):
-        a.act()     # Play one time
-        greedy_avg[i] = a.overall_reward / sum(a.pulls)
-    total_greedy_avg = map(operator.add, total_greedy_avg, greedy_avg)
-
-    a = EpsilonGreedy(epsilon, b)
-    for i in range(iterations):
-        a.act()
-        e_greedy_avg[i] = a.overall_reward / sum(a.pulls)
-    total_e_greedy_avg = map(operator.add, total_e_greedy_avg, e_greedy_avg)
-
-    a = Softmax(tau, b)
-    for i in range(iterations):
-        a.act()
-        softmax_avg[i] = a.overall_reward / sum(a.pulls)
-    total_softmax_avg = map(operator.add, total_softmax_avg, softmax_avg)
-total_greedy_avg = [e / bandits for e in total_greedy_avg]
-total_e_greedy_avg = [e / bandits for e in total_e_greedy_avg]
-total_softmax_avg = [e / bandits for e in total_softmax_avg]
-x = [i for i in range(iterations)]
-pp.plot(x, total_greedy_avg, aa=False, label="greedy")
-pp.plot(x, total_e_greedy_avg, aa=False, label="$\epsilon$-greedy ($\epsilon$ = " + str(epsilon) + ")")
-pp.plot(x, total_softmax_avg, aa=False, label="softmax ($\\tau$ = " + str(tau) + ")")
+x = np.arange(iterations)
+pp.plot(x, avg[0], aa=False, linewidth=1, label="greedy")
+pp.plot(x, avg[1], aa=False, linewidth=1, label="$\epsilon$-greedy ($\epsilon$ = " + str(epsilon) + ")")
+pp.plot(x, avg[2], aa=False, linewidth=1, label="softmax ($\\tau$ = " + str(tau) + ")")
+pp.gcf().set_size_inches(12, 6)
 pp.legend(loc="lower right")
 pp.xlabel("Games")
 pp.ylabel("Average reward per game")

actors.py
@@ -1,15 +1,16 @@
 import random
 import math
+import numpy as np
 
 
 class Actor:
-    def __init__(self, bandit):
+    def __init__(self, bandit, initial_estimate):
+        self.initial_estimate = initial_estimate
         self.bandit = bandit
-#        self.estimates = [max(bandit.means) for _ in range(bandit.arms)]
-        self.estimates = [0 for _ in range(bandit.arms)]
-        self.total_rewards = [0 for _ in range(bandit.arms)]
-        self.pulls = [0 for _ in range(bandit.arms)]
-        self.overall_reward = 0
+        self.estimates = np.empty(bandit.arms)
+        self.estimates.fill(initial_estimate)
+        self.total_rewards = np.zeros(bandit.arms)
+        self.pulls = np.zeros(bandit.arms, dtype=np.int64)
 
     def _choose_arm(self):
         return 0
@@ -20,38 +21,42 @@ class Actor:
         self.total_rewards[arm] += rew
         self.pulls[arm] += 1
         self.estimates[arm] = self.total_rewards[arm] / self.pulls[arm]
-        self.overall_reward += rew
-        return [arm, rew]
+        return rew
+
+    def set_bandit(self, bandit):
+        self.bandit = bandit
+        self.estimates.fill(self.initial_estimate)
+        self.total_rewards.fill(0)
+        self.pulls.fill(0)
 
 
 class Greedy(Actor):
-    def __init__(self, bandit):
-        Actor.__init__(self, bandit)
+    def __init__(self, bandit, initial_estimate):
+        Actor.__init__(self, bandit, initial_estimate)
 
     def _choose_arm(self):
-        return self.estimates.index(max(self.estimates))
+        return self.estimates.argmax()
 
 
 class EpsilonGreedy(Actor):
-    def __init__(self, epsilon, bandit):
+    def __init__(self, epsilon, bandit, initial_estimate):
         self.epsilon = epsilon
-        Actor.__init__(self, bandit)
+        Actor.__init__(self, bandit, initial_estimate)
 
     def _choose_arm(self):
         if random.uniform(0, 1) < self.epsilon:
             return random.randint(0, self.bandit.arms - 1)       # Explore
         else:
-            return self.estimates.index(max(self.estimates))     # Exploit current knowledge
+            return self.estimates.argmax()                       # Exploit current knowledge
 
 
 class Softmax(Actor):
-    def __init__(self, temperature, bandit):
+    def __init__(self, temperature, bandit, initial_estimate):
         self.temperature = temperature
-        self.p = [1 / bandit.arms for _ in range(bandit.arms)]
-        Actor.__init__(self, bandit)
+        self.p = np.empty(bandit.arms)
+        Actor.__init__(self, bandit, initial_estimate)
 
     def _choose_arm(self):
-        # TODO: Do not recalculate whole 'p' list every time. Just update what needs to be updated
         s = 0
         for i in range(self.bandit.arms):
             s += math.exp(self.estimates[i] / self.temperature)

bandit.py
@@ -1,10 +1,11 @@
 import random
+import numpy as np
 
 
 class Bandit:
     def __init__(self, arms):
         self.arms = arms
-        self.means = [random.gauss(0, 1) for _ in range(arms)]
+        self.means = np.array([random.gauss(0, 1) for _ in range(arms)])
 
     def pull(self, arm):
         return random.gauss(self.means[arm], 1)